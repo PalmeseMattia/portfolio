b3ba31a6b0a3d4bc1628e1460e58def285a99507
Palmese Davide Mattia <palmesedavidemattia@gmail.com>
2022-06-28 23:23:29 +0200

    first commit i think

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Container, Navbar, Stack } from 'react-bootstrap'
+import Logo from './assets/logo.png'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='main--page'>
+      {/* NAVBAR COMPONENT */}
+      <Navbar bg='light'>
+            <img 
+              src={Logo}
+              width="80"
+              height="50"
+              className="d-inline-block align-top ms-3"></img>
+            <Button className='ms-auto me-3'>LET'S TALK</Button>
+      </Navbar>
 
-export default App;
+      {/* MAIN PAGE COMPONENT */}
+      <Stack direction="horizontal">
+        <h3>ciao</h3>
+      </Stack>
+    </div>
+  )
+}
